820434d84b9bf95e3a0dbcc23ad2658316e019e8
Ari Chivukula <ari@chivuku.la>
2018-07-25 19:45:24 -0700

    Update cli.ts

--- source/cli.ts
+++ source/cli.ts
@@ -1,1 +1,21 @@
 #!/usr/bin/env node
+
+import { readFileSync } from "fs";
+
+interface EnvConfigs {
+    [name: string]: EnvConfig;
+}
+
+interface EnvConfig {
+  description = string;
+}
+
+const configs: EnvConfigs = JSON.parse(readFileSync(".picturesque", "ascii"));
+
+Object.keys(configs).map(
+  (env_key: string) => {
+    if (process.env[env_key] === undefined || process.env[env_key].length < 1) {
+      throw new Error(`Missing ${env_key} (${configs[env_key].description}`);
+    }
+  },
+);
